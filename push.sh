3515030286657862e722a639a5ea08085c906bdb
ApiX <edosetiabudi2@protonmail.com>
2022-03-23 11:56:52 -0700

    Make it better

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git config --global user.email "101666083+vxzdev@users.noreply.github.com"
-git config --global user.name "VxzDev"
+git config --global user.email "edosetiabudi2@protonmail.com"
+git config --global user.name "ApiX"
 git add .
 git commit -am "Make it better"
 git push origin master --force
