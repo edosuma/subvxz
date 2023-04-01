784ee20b4d0854f4f7476426270d46612e05bbc6
ApiX <edosetiabudi2@protonmail.com>
2023-04-01 11:21:24 -0700

    Make it better

---
+++ push - manual.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global user.email "edosetiabudi2@protonmail.com"
+git config --global user.name "ApiX"
+git add .
+git commit -am "Make it better"
+git push origin master --force
